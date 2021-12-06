1b60959d0ff364cba943f6de89fa46f4e0ee094c
gians <giansluca@gmail.com>
2021-12-06 19:55:00 +0100

    refactoring

--- env/env.sh
+++ env/env.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 export POSTGRES_USER=postgres
-export POSTGRES_PASS=12345x
-export MONGO_USER=spring_user
-export MONGO_PASS=12345009
+export POSTGRES_PASS=12345
 export MYSQL_ROOT_USER=root
 export MYSQL_ROOT_PASSWORD=12345
 export MYSQL_DATABASE=spring-boot-demo
