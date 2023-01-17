4a75ffc7fbe3c1706a1751915302f3f2c20a296d
gians <giansluca@gmail.com>
2023-01-17 18:12:56 +0100

    refactoring

--- src/test/java/org/gmdev/service/school/StudentCourseServiceTest.java
+++ src/test/java/org/gmdev/service/school/StudentCourseServiceTest.java
@@ -1,5 +1,6 @@
 package org.gmdev.service.school;
 
+import org.gmdev.api.model.school.CourseStudentApiRes;
 import org.gmdev.api.model.school.StudentCourseApiRes;
 import org.gmdev.dao.school.CourseRepository;
 import org.gmdev.dao.school.StudentCourseRepository;
@@ -10,10 +11,8 @@ import org.gmdev.model.entity.school.StudentCourse;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.transaction.Transactional;
 import java.time.ZoneId;
@@ -85,6 +84,45 @@ class StudentCourseServiceTest {
         assertThat(studentCoursesS2.get(0).getTitle()).isEqualTo("Fishing course");
 
         assertThat(studentCoursesS3).isEmpty();
+    }
+
+    @Test
+    void itShouldFindCourseStudents() {
+        // Given
+        List<Student> students = getFakeStudentEntities();
+        List<Course> courses = getFakeCourseEntities();
+        studentRepository.saveAll(students);
+        courseRepository.saveAll(courses);
+
+        Student student1 = students.get(0);
+        Student student2 = students.get(1);
+        Course course1 = courses.get(0);
+        Course course2 = courses.get(1);
+        Course course3 = courses.get(2);
+
+        ZonedDateTime now = ZonedDateTime.now(ZoneId.of("Z"));
+        StudentCourse mark1 = new StudentCourse(student1, course1, 7, now, now);      // Mark <--> Kitchen course
+        StudentCourse mark2 = new StudentCourse(student1, course2, 5, now, now);      // Mark <--> Fishing course
+        StudentCourse steven1 = new StudentCourse(student2, course2, 9, now, now);    // Steven <--> Fishing course
+        studentCourseRepository.saveAllAndFlush(List.of(mark1, mark2, steven1));
+
+        // When
+        List<CourseStudentApiRes> courseStudentsC1 = underTest.getCourseStudents(course1.getId());
+        List<CourseStudentApiRes> courseStudentsC2 = underTest.getCourseStudents(course2.getId());
+        List<CourseStudentApiRes> courseStudentsC3 = underTest.getCourseStudents(course3.getId());
+
+        // Then
+        assertThat(courseStudentsC1).isNotEmpty().hasSize(1);
+        assertThat(courseStudentsC1.get(0).getStudentId()).isEqualTo(student1.getId());
+        assertThat(courseStudentsC1.get(0).getName()).isEqualTo("Mark");
+
+        assertThat(courseStudentsC2).isNotEmpty().hasSize(2);
+        assertThat(courseStudentsC2.get(0).getStudentId()).isEqualTo(student1.getId());
+        assertThat(courseStudentsC2.get(0).getName()).isEqualTo("Mark");
+        assertThat(courseStudentsC2.get(1).getStudentId()).isEqualTo(student2.getId());
+        assertThat(courseStudentsC2.get(1).getName()).isEqualTo("Steven");
+
+        assertThat(courseStudentsC3).isEmpty();
     }
 
     private List<Student> getFakeStudentEntities() {
