4a181ed3ba5cb520dc45e800f5b3e8fc08d6b89e
gians <giansluca@gmail.com>
2023-02-18 20:00:48 +0100

    update

--- src/test/java/org/gmdev/setup/MongoDBTestContainerSetup.java
+++ src/test/java/org/gmdev/setup/MongoDBTestContainerSetup.java
@@ -1,17 +1,14 @@
 package org.gmdev.setup;
 
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @Testcontainers
-@AutoConfigureTestDatabase(replace = NONE)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public class MongoDBTestContainerSetup {
 
