682c04300a3575a6e6ca2e5633c2a8a27a7f563c
gians <giansluca@gmail.com>
2026-03-09 17:29:31 +0100

    chore: test workflow 1

--- src/main/java/org/gmdev/api/AsyncController.java
+++ src/main/java/org/gmdev/api/AsyncController.java
@@ -20,7 +20,7 @@ public class AsyncController {
     @PostMapping("/slow-operation")
     public void slowOperation() {
         try {
-            log.info("Incoming call to [AsyncController - slowOperation] 3");
+            log.info("Incoming call to [AsyncController - slowOperation] 4");
             Thread.sleep(8000);
             log.info("Finish [AsyncController - slowOperation]");
         } catch (InterruptedException e) {
